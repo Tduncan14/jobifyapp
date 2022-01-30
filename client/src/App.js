2be951bd8297c6eebd8b3b94fb88208330889c5d
tduncan14 <nbatre1@yahoo.com>
2022-01-30 15:30:08 -0800

    keep moving forward

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,30 @@
 
+import Landing from './pages/Landing';
+
 
 function App() {
   return (
-    <div className="App">
-     hello
-    </div>
+
+    <main>
+      <nav>
+        <img src={logo} alt="jobify" className="logo"/>
+      </nav>
+
+      <div className="container page">
+        <div className="info">
+          <h1>Job<span>Tracking</span> App</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nisi debitis velit aliquid error vitae eligendi delectus maxime soluta est labore beatae molestias repudiandae provident et quas nemo, minima vel consectetur.
+          Aut quisquam architecto aspernatur! Harum velit enim quo, et, veniam consectetur corrupti beatae pariatur rerum accusamus esse quisquam laboriosam aperiam nemo, eaque rem qui quis iste aut provident nostrum. Debitis!
+        </p>
+
+        <button className="btn btn-hero">Login/Register</button>
+        </div>
+          <img src={main} alt="job hunt" className="img"/>
+      </div>
+    </main>
+  
+
   );
 }
 
---
+++ client/src/pages/Landing.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import logo  from '../assets/images/logo.svg';
+import main from '../assets/images/main.svg';
+
+
+
+const Landing = () => {
+
+
+
+    return(
+        <div>
+            <h4>Landing Page</h4>
+
+        </div>
+    )
+
+
+
+
+
+
+
+
+}
+
+export default Landing;

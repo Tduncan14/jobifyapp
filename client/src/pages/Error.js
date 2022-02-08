b5cf1455c85f387ed89418f830376b099c93bfdc
tduncan14 <nbatre1@yahoo.com>
2022-02-08 14:51:01 -0800

    keep moving forward

--- client/src/pages/Error.js
+++ client/src/pages/Error.js
@@ -1,4 +1,6 @@
-
+import {Link} from 'react-router-dom';
+import img from '../assets/images/not-found.svg';
+import Wrapper from '../wrappers/ErrorPage';
 
 const Error = () => {
 
--- client/src/wrappers/testing.js
+++ client/src/wrappers/testing.js
@@ -1,56 +1,40 @@
-import styled from 'styled-components';
+import styled from 'styled-components'
 
 const Wrapper = styled.main`
-
-nav{
-    width:var(--fluid-width);
-    max-width:var(--max-width);
-    margin:0 auto;
+  nav {
+    width: var(--fluid-width);
+    max-width: var(--max-width);
+    margin: 0 auto;
     height: var(--nav-height);
-    display:flex;
-    align-items:center;
-
-}
-  
-  
-.page{
-    min-height: calc(100vh - var(--nav));
-    display:grid;
-    align-items:center;
-    margin-top:-3rem;
-
-}
-
-
-  h1{
-      font-weight:700;
-
-
-      span{
-          color:var(--primary-500);
-      }
+    display: flex;
+    align-items: center;
   }
-
-  p{
-      color:var(--grey-600);
+  .page {
+    min-height: calc(100vh - var(--nav-height));
+    display: grid;
+    align-items: center;
+    margin-top: -3rem;
   }
-    
-
-  .main-img{
-      display:none;
+  h1 {
+    font-weight: 700;
+    span {
+      color: var(--primary-500);
+    }
   }
-
-  @media only screen and (min-width: 992px) {
-      .page{
-          grid-template-columns: 1fr 1fr;
-          column-gap:3rem;
-      }
-
-
-      .main-img{
-          display:block;
-      }
+  p {
+    color: var(--grey-600);
+  }
+  .main-img {
+    display: none;
+  }
+  @media (min-width: 992px) {
+    .page {
+      grid-template-columns: 1fr 1fr;
+      column-gap: 3rem;
+    }
+    .main-img {
+      display: block;
+    }
   }
 `
-
 export default Wrapper

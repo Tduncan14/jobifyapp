92c2e2eb4a7a8307c342afe953ac47a8eb52d311
tduncan14 <nbatre1@yahoo.com>
2022-03-23 02:00:35 -0700

    keep moving forward

--- controller/authController.js
+++ controller/authController.js
@@ -13,6 +13,17 @@ const register =  async (req,res) => {
       const {name,email,password} = req.body;
 
 
+    //   find email
+
+
+    const userAlreadyExist = await UserSchema.findOne({email});
+
+
+    if(userAlreadyExist){
+        throw new Error('Email are in use');
+    }
+
+
       if(!name || !email || !password){
           throw new BadRequestError('please provide all values')
       }
--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -25,7 +25,7 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
   }
 
-  res.status(500).json({msg:err})
+  res.status(defaultError.statusCode).json({msg:defaultError.msg})
 }
 
 export default errorHandlerMiddleware
